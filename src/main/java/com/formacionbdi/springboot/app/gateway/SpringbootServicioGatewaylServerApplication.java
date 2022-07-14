d49d3d25a07f7c6d227e15fc2422ed37276e4371
miguelalonsocoba <miguetheking53@gmail.com>
2022-07-14 17:18:51 -0500

    Inicializando proyecto

---
+++ src/main/java/com/formacionbdi/springboot/app/gateway/SpringbootServicioGatewaylServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioGatewaylServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioGatewaylServerApplication.class, args);
+	}
+
+}
